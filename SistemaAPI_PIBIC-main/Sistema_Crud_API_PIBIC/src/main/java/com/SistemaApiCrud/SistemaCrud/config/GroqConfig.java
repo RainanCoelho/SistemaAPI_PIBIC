c344b4047a987dca2555302d8a55cac8ba2db9a1
RainanCoelho <raiaraujocoelho@gmail.com>
2026-07-01 16:13:05 -0300

    Commit da integração da IA

--- SistemaAPI_PIBIC-main/Sistema_Crud_API_PIBIC/src/main/java/com/SistemaApiCrud/SistemaCrud/config/GroqConfig.java
+++ SistemaAPI_PIBIC-main/Sistema_Crud_API_PIBIC/src/main/java/com/SistemaApiCrud/SistemaCrud/config/GroqConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GroqConfig {
 
+
     @Bean
     public OkHttpClient okHttpClient(){
         return new OkHttpClient();
